fb0588150097b51c9983648a30af4384a7907f69
Kevin Wessa <kjwessa@gmail.com>
2024-02-07 10:31:06 -0600

    Added footer text with hours and button using conditional rendering

--- src/index.js
+++ src/index.js
@@ -94,13 +94,20 @@ function Pizza(props) {
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 12;
+  const openHour = 8;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
   console.log(isOpen);
 
   return (
-    <footer className="footer">{new Date().toLocaleTimeString()} We're currently open! </footer>
+    <footer>
+      {isOpen && (
+        <div className="order">
+          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+          <button className="btn">Order</button>
+        </div>
+      )}
+    </footer>
   );
 }
 
